1ddae70e91a47d228625d2b212195213dcf5908f
GrulsuitG <95327051+GrulsuitG@users.noreply.github.com>
2022-06-02 11:31:59 +0900

    query update

---
+++ Crawling/model.py
@@ -0,0 +1,9 @@
+import pymysql
+import numpy as np
+# import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
