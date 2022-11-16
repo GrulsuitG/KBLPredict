e84f1d587f3c656b676fb002c55cbbe40ac5a3ba
kim.beomjin <kim.beomjin@worksmobile.com>
2022-11-16 20:37:28 +0900

    제출 파일 정리

--- Crawling/DB.py
+++ Crawling/DB.py
@@ -16,24 +16,6 @@ class HYGPDB:
                                     )
         self.conn = self.engine.connect()
 
-    # def read_DB_info(self):
-    #     with open('config.json', 'r') as f:
-    #         config = json.load(f)['DB']
-
-    #     return config
-
-    # def set_page_DB(self):
-    #     config = self.read_DB_info()
-
-    #     self.engine = create_engine(("mysql+pymysql://" +
-    #                                  config['user'] + ":%s@" +
-    #                                  config['host'] + ":3306" + "/" +
-    #                                  config['db'] + "?charset=utf8") % quote(config['password']),
-    #                                 encoding='utf-8'
-    #                                 )
-    #     self.conn = self.engine.connect()
-    #     print("Connect!")
-
 class MYDB:
     def __init__(self):
       with open('config.json', 'r') as f:
